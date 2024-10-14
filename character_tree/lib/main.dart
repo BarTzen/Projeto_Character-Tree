f281d2ec75e862911dd486a79b28849ea5eedd70
Andre <andrearaujo@sou.faccat.br>
2024-10-14 09:52:12 -0300

    Firebase Conectado ao App

--- character_tree/lib/main.dart
+++ character_tree/lib/main.dart
@@ -1,20 +1,42 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'view/login_screen.dart';
 import 'view/register_screen.dart';
 import 'viewmodel/login_viewmodel.dart';
 import 'viewmodel/register_viewmodel.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
+Future<void> main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Garante a inicialização do Flutter
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    if (kDebugMode) {
+      print('Erro ao inicializar o Firebase: $e');
+    }
+  }
+  runApp(MyAppWithProviders());
+}
+
+class MyAppWithProviders extends StatelessWidget {
+  const MyAppWithProviders({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => LoginViewModel()),
         ChangeNotifierProvider(create: (_) => RegisterViewModel()),
       ],
-      child: MyApp(),
-    ),
-  );
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -23,11 +45,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      theme: ThemeData.light(), // Tema claro
+      darkTheme: ThemeData.dark(), // Tema escuro
+      themeMode: ThemeMode.system, // Alterna de acordo com o sistema
+      initialRoute: '/', // Rota inicial
       routes: {
-        '/': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
+        '/': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
       },
+      debugShowCheckedModeBanner: false, // Remove o banner de debug
     );
   }
 }
