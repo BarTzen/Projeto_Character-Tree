f8482c62f3114a7e754ebf20eaef1e766fc2d6ab
Luciano <79113259+Luciano66641@users.noreply.github.com>
2024-11-16 16:36:04 -0300

    Atualizando arquivo home_screen

--- character_tree/lib/view/home/home_screen.dart
+++ character_tree/lib/view/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:character_tree/models/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../widgets/user_avatar.dart';
@@ -14,6 +15,15 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await AuthService().sair();  // Chama o método sair() do seu AuthService
+              Navigator.pushReplacementNamed(context, '/'); // Navega para a tela de login
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
